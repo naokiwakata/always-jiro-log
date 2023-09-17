4c157fb4e9a734d857c9848f5bf936813d919379
naokiwakata <rockwakata15@gmail.com>
2023-09-17 16:27:20 +0900

    first commit

---
+++ src/main/kotlin/com/naokiwakata/alwaysjirolog/AlwaysJiroLogApplication.kt
@@ -0,0 +1,11 @@
+package com.naokiwakata.alwaysjirolog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlwaysJiroLogApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlwaysJiroLogApplication>(*args)
+}
---
+++ src/test/kotlin/com/naokiwakata/alwaysjirolog/AlwaysJiroLogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.naokiwakata.alwaysjirolog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlwaysJiroLogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
